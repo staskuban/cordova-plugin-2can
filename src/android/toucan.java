4cf7ecaa9298eca2375968ceb97b4047f150baad
Stanislav Sidoryuk <staskuban@ya.ru>
2018-07-26 17:18:09 +0300

    fix successful payment crash

--- src/android/toucan.java
+++ src/android/toucan.java
@@ -12,90 +12,84 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 
+import java.util.Iterator;
+
+import android.net.Uri;
+
+import java.lang.reflect.Field;
+
+import android.content.ActivityNotFoundException;
+import android.util.Log;
+import android.os.Bundle;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class toucan extends CordovaPlugin {
-CallbackContext callbackctx;
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        this.callbackctx = callbackContext;
-        if (action.equals("pay")) {
-           JSONObject obj = args.getJSONObject(0);
-            this.pay(obj);
-            return true;
-        }
-        return false;
-    }
+	private static final int PAYMENT_RESULT_CODE = 0x101010;
+	CallbackContext callbackctx;@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		this.callbackctx = callbackContext;
+		if (action.equals("pay")) {
+			JSONObject obj = args.getJSONObject(0);
+			this.pay(obj);
+			return true;
+		}
+		return false;
+	}
 
-   /**
+	/**
      * toucanpayment
      */
-    private void pay(JSONObject obj) {
-        JSONObject res = new JSONObject();
-        try {
-            if (obj != null) {
-               Context context = cordova.getActivity();
-               Intent intent = new Intent("ru.toucan.PAYMENT");
-               intent.putExtra("PackageName", obj.getString("packagename"));
-               intent.putExtra("SecureCode", obj.getString("securesode"));
-               intent.putExtra("Amount", obj.getInt("amount"));
-               intent.putExtra("Description", obj.getString("description"));
+	private void pay(JSONObject obj) {
+		JSONObject res = new JSONObject();
+		try {
+			if (obj != null) {
+				Context context = cordova.getActivity();
+				Intent intent = new Intent("ru.toucan.PAYMENT");
+				intent.putExtra("PackageName", obj.getString("packagename"));
+				intent.putExtra("SecureCode", obj.getString("securecode"));
+				intent.putExtra("Amount", obj.getInt("amount"));
+				intent.putExtra("Description", obj.getString("description"));
+				intent.putExtra("Response", "SHORT");
 
+				cordova.setActivityResultCallback(this);
+				cordova.getActivity().startActivityForResult(intent, 4);
+			} else {
+				res.put("success", new Boolean(false));
+				res.put("stack", "pay method");
+				this.callbackctx.error(res);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+			this.callbackctx.error(new JSONObject());
+		}
+	}
 
-
-
-                cordova.setActivityResultCallback(this);
-                cordova.getActivity().startActivityForResult(intent, 4);
-            } else {
-               res.put("success", new Boolean(false));
-               this.callbackctx.error(res);
-            }
-        } catch (Exception e) {
-          this.callbackctx.error(new JSONObject());
-        }
-    }
-
-        /**
+	/**
          * Обработка результата из интента
          *
          * @param requestCode
          * @param resultCode
          * @param data
          */
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        JSONObject res = new JSONObject();
-        try {
-            JSONObject json = new JSONObject();
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		JSONObject json = new JSONObject();
+		try {
 
-            for (String key : data.getExtras().keySet()) {
-                Object value = data.getExtras().get(key);
-                if (value != null) {
-                    json.put(key, value);
-                }
-            }
-            if (requestCode == 4) {
-                res.put("action", "ru.toucan.PAYMENT");
-                if (resultCode == -1) {
-                    res.put("success", new Boolean(true));
-                } else if (resultCode == 0) {
-                    res.put("success", new Boolean(false));
-                }
-                res.put("additional_data", json);
-                   this.callbackctx.success(res);
-            }
-            else {
-            res.put("success", new Boolean(false));
-            this.callbackctx.error(res);
-            }
+			Log.d("2can", "got result ");
 
-        } catch (JSONException e) {
-
-           this.callbackctx.error(new JSONObject());
-        }
-
-
-    }
+			for (String key: data.getExtras().keySet()) {
+				Object value = data.getExtras().get(key);
+				if (value != null) {
+					json.put(key, value);
+				}
+			}
+			this.callbackctx.success(json);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
